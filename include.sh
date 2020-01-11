21d2367b3ca174b0b1f53066887528dba0f7b134
BarbzYHOOL <professor_barbz@gmx.com>
2020-01-11 17:58:56 +0100

    chore(sql): Default dir structure for the db_assembler.sh
    - Actually (early 2020) it's not required to follow this structure, but it's recommended as it's simple and logical and covers all needs

--- include.sh
+++ include.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## GETS THE CURRENT MODULE ROOT DIRECTORY
 MOD_SKELETON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 source $MOD_SKELETON_ROOT"/conf/conf.sh.dist"
