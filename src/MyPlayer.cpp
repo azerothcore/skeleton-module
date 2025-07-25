15def803019847350bf8c25fde1f22c82f18415f
Tereneckla <Tereneckla@pm.me>
2025-07-25 17:52:48 +0200

    Cache the config

--- src/MyPlayer.cpp
+++ src/MyPlayer.cpp
@@ -2,15 +2,36 @@
  * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
  */
 
-#include "ScriptMgr.h"
-#include "Player.h"
-#include "Config.h"
 #include "Chat.h"
+#include "Config.h"
+#include "ConfigValueCache.h"
+#include "Player.h"
+#include "ScriptMgr.h"
 
 enum MyPlayerAcoreString
 {
     HELLO_WORLD = 35410
 };
+
+enum class MyConfig
+{
+    ENABLED,
+
+    NUM_CONFIGS,
+};
+
+class MyConfigData : public ConfigValueCache<MyConfig>
+{
+public:
+    MyConfigData() : ConfigValueCache(MyConfig::NUM_CONFIGS) { };
+
+    void BuildConfigCache() override
+    {
+        SetConfigValue<bool>(MyConfig::ENABLED, "MyModule.Enable", true);
+    }
+};
+
+static MyConfigData myConfigData;
 
 // Add player scripts
 class MyPlayer : public PlayerScript
@@ -25,8 +46,22 @@ public:
     }
 };
 
+class MyWorldScript : public WorldScript
+{
+public:
+MyWorldScript() : WorldScript("MyWorldScript", {
+        WORLDHOOK_ON_BEFORE_CONFIG_LOAD
+    }) { }
+
+    void OnBeforeConfigLoad(bool reload) override
+    {
+        myConfigData.Initialize(reload);
+    }
+};
+
 // Add all scripts in one
 void AddMyPlayerScripts()
 {
     new MyPlayer();
+    new MyWorldScript();
 }
